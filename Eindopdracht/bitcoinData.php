baa96dce563b7a6c4ae9f185905175af159a7b38
sidrocva <2055851@talnet.nl>
2022-03-10 11:02:13 +0100

    eindopdr php1 klaar

--- Eindopdracht/bitcoinData.php
+++ Eindopdracht/bitcoinData.php
@@ -18,7 +18,7 @@
 /* 
  * Opdracht 2: Zet hier de $buttons array neer 
  */
-
+$buttons = [25,50.5,75];
 
 /******************************************* OPDRACHT 2 doe je hierboven *******************************************/
 /*******************************************************************************************************************/
@@ -38,6 +38,7 @@ function calculateBitcoinAmount()
 {
     if (isset($_GET['euro'])) {
         $euro = (int)$_GET['euro']; //Dit blokje code (if-statement) kun je laten staan.
+        return  $euro/getBitcoinPrice();
     }
 
     //SCHRIJF JE CODE.
@@ -74,7 +75,7 @@ $transactions = [
 if (isset($_GET['euro'])) {
 
     $euro = $_GET['euro']; //deze regel laten staan
-
+    array_push($transactions,[getBitcoinPrice(),$euro,calculateBitcoinAmount()]);
     /* SCHRIJF HIER JE CODE.
      * Schrijf hieronder de code om de gekochte bitcoin aan de array $transactions toe te voegen.
      * Je voegt een bitcoinprijs toe, de prijs in euro's en het aantal gekochte bitcoins.
